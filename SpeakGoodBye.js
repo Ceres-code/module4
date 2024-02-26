8880305c8174f0c319d303d3fb2a17b2725af2cb
Ceres-code <158100161+Ceres-code@users.noreply.github.com>
2024-02-26 16:14:19 +0100

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,14 +1,14 @@
-// Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
+
 (function (window) {
-  var byeSpeaker = {}; // Create an object
+  var byeSpeaker = {}; 
 
-  var speakWord = "Good Bye"; // Do not attach speakWord to byeSpeaker
+  var speakWord = "Good Bye"; 
 
-  // Attach the speak method to byeSpeaker
+  
   byeSpeaker.speak = function (name) {
     console.log(speakWord + " " + name);
   };
 
-  // Expose byeSpeaker to the global scope
+  
   window.byeSpeaker = byeSpeaker;
 })(window);
