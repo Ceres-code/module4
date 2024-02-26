8ec1a564634d0afbc601189aaeb4a9f76654130e
Ceres-code <158100161+Ceres-code@users.noreply.github.com>
2024-02-26 16:14:46 +0100

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,15 +1,15 @@
-// Wrap the entire contents of SpeakHello.js inside of an IIFE
+
 (function (window) {
-  var helloSpeaker = {}; // Create an object
+  var helloSpeaker = {};
 
-  var speakWord = "Hello"; // Do not attach speakWord to helloSpeaker
+  var speakWord = "Hello";
 
-  // Attach the speak method to helloSpeaker
+  
   helloSpeaker.speak = function (name) {
     console.log(speakWord + " " + name);
   };
 
-  // Expose helloSpeaker to the global scope
+  
   window.helloSpeaker = helloSpeaker;
 })(window);
 
